7247f2f52f057482455fff739de33afbb50c5844
andrew54068 <andrew0424718012@yahoo.com.tw>
2018-07-20 00:01:35 +0800

    [error] test for first condition fail

--- Passphrase/PassphraseTests/PassphraseTests.swift
+++ Passphrase/PassphraseTests/PassphraseTests.swift
@@ -11,25 +11,31 @@ import XCTest
 
 class PassphraseTests: XCTestCase {
     
+    var passphrase: Passphrase?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        passphrase = Passphrase()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        passphrase = nil
         super.tearDown()
     }
     
+    func testFirstCondition() {
+        let result = passphrase.shiftWords(words: "abc")
+        assert(result = "cba", "FirstCondition incorrect")
+    }
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
+        
         self.measure {
-            // Put the code you want to measure the time of here.
+            
         }
     }
     
