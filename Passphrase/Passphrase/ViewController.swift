025ebbdfd6a8729cbf6cb38c87c0f8e4b43a45d9
andrew54068 <andrew0424718012@yahoo.com.tw>
2018-07-19 23:53:06 +0800

    init

---
+++ Passphrase/Passphrase/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Passphrase
+//
+//  Created by kidnapper on 19/07/2018.
+//  Copyright © 2018 kidnapper. All rights reserved.
+//
+
+import Foundation
+
+class Passphrase: NSObject {
+    
+    
+
+}
+
